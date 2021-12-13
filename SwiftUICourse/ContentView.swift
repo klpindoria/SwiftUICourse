2b12bd63011b875aa4564289193f72156d7508a5
klpindoria <44475172+klpindoria@users.noreply.github.com>
2021-12-13 22:22:44 +0000

    Initial Commit

---
+++ SwiftUICourse/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUICourse
+//
+//  Created by Ketan Pindoria on 13/12/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftUICourse/SwiftUICourseApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUICourseApp.swift
+//  SwiftUICourse
+//
+//  Created by Ketan Pindoria on 13/12/2021.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUICourseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
