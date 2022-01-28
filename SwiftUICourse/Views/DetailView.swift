da2f39db4b04b58eea96e82a5ceef1f2c4916ff9
klpindoria <44475172+klpindoria@users.noreply.github.com>
2022-01-28 00:10:47 +0000

    Display Dynamic Mock Data for DetailView

--- SwiftUICourse/Views/DetailView.swift
+++ SwiftUICourse/Views/DetailView.swift
@@ -9,7 +9,7 @@ import SwiftUI
 
 struct DetailView: View {
     
-    var item: Item = items[2]
+    var item: Item = items[0]
     
     var body: some View {
         ScrollView {
@@ -38,11 +38,11 @@ struct DetailView: View {
             .background(item.gradient)
             
             VStack(alignment: .leading, spacing: 16.0) {
-                Text("SwiftUI is hands-down the best way for designers to take a first step into code")
+                Text(item.detailTitle)
                     .font(.title2)
-                Text("This course")
+                Text(item.detailSubHeading)
                     .font(.title3).bold()
-                Text("This course is unlike any other. We care about design and want to make sure that you get better at it in the process. It was written for designers and developers who are passionate about collaborating and building real apps for iOS and macOS. While it's not one codebase for all apps, you learn once and can apply those techniques and controls to all platforms with incredible quality, consistency and performance. It's beginner friendly, but it's also packed with design tricks and efficient workflows for building great user interfaces and interactions.")
+                Text(item.detailBody)
                     .font(.body)
             }
             .padding()
